45d746f47fd008cad1a46ef946a659c32af4e5e7
Krist Wongsuphasawat <krist.wongz@gmail.com>
2017-02-10 23:08:52 -0800

    add vis code

--- src/vis/SentenTreeVis.js
+++ src/vis/SentenTreeVis.js
@@ -1,5 +1,6 @@
 import { SvgChart, helper } from 'd3kit/dist/d3kit-es.js';
-import 'webcola/WebCola/cola.min.js';
+
+const cola = require('webcola');
 
 class SentenTreeVis extends SvgChart {
   static getDefaultOptions() {
@@ -15,7 +16,7 @@ class SentenTreeVis extends SvgChart {
   constructor(element, options) {
     super(element, options);
 
-    // this.layers.create([]);
+    this.layers.create(['link', 'node']);
 
     this.visualize = this.visualize.bind(this);
     this.on('data', this.visualize);
@@ -24,18 +25,98 @@ class SentenTreeVis extends SvgChart {
 
     console.log('cola', cola);
 
-    // this.colaAdaptor = cola.d3adaptor()
-    //   .flowLayout('x')
-    //   .avoidOverlaps(true);
+    this.colaAdaptor = cola.d3adaptor()
+      .flowLayout('x')
+      .avoidOverlaps(true)
+      .jaccardLinkLengths()
+      .linkDistance(5)
+      //.symmetricDiffLinkLengths()
+  }
 
+  renderNodes(nodes) {
+    const sUpdate = this.layers.get('node').selectAll('g')
+      .data(nodes, n => n.data.id);
+
+    sUpdate.exit().remove();
+
+    const sEnter = sUpdate.enter().append('g')
+      .attr('transform', d => `translate(${d.x}, ${d.y})`)
+
+    sEnter.append('text')
+      .text(d => d.data.entity)
+
+
+  }
+
+  renderLinks(links) {
+    const sUpdate = this.layers.get('link').selectAll('line')
+      .data(links);
+
+    sUpdate.exit().remove();
+
+    sUpdate.enter().append('line')
+      .attr('x1', link => link.sourceNode.x)
+      .attr('y1', link => link.sourceNode.y)
+      .attr('x2', link => link.targetNode.x)
+      .attr('y2', link => link.targetNode.y)
   }
 
   visualize() {
     if (!this.hasData() || !this.hasNonZeroArea()) return;
 
-    const data = this.data();
+    const graph = this.data();
 
+    this.colaAdaptor
+      .nodes(graph.nodes)
+      .links(graph.links)
+      .constraints(graph.getConstraints())
+      .start(10,10,10);
 
+    this.renderNodes(graph.nodes);
+    this.renderLinks(graph.links);
+
+    colaAdaptor.on("tick", function (event) {
+      // draw directed edges with proper padding from node centers
+      path.each(function (d) {
+        var deltaX = d.target.x - d.source.x;
+        var deltaY = d.target.y - d.source.y;
+        var dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        dist = dist===0 ? 1 : dist;
+
+        var normX = deltaX / dist,
+            normY = deltaY / dist;
+
+        d.s = {'y':d.source.x + (d.source.bounds.width()/2 * normX),
+                'x':d.source.y + (d.source.bounds.height()/2 * normY)};
+        d.t = {'y':d.target.x - (d.target.bounds.width()/2 * normX),
+                'x':d.target.y - (d.target.bounds.height()/2 * normY)};
+      });
+
+      path.attr('d', diagonal);
+
+      box
+        .attr("x", function (d) { return d.bounds.x; })
+        .attr("y", function (d) { return d.bounds.y; })
+        .attr("width", function (d) { return d.bounds.width(); })
+        .attr("height", function (d) { return d.bounds.height(); });
+
+      label
+        .attr("x", function (d) { return d.x - d.bounds.width()/2; })
+        .attr('dy', '.28em')
+        .attr("y", function (d) { return d.y; });
+
+      // crop SVG
+      var gbbox = nodeGroup.node().getBBox();
+      if( Math.abs(gbbox.width - width) > 5 || Math.abs(gbbox.height - height) > 5 ) {
+        zoom.translate([5 - gbbox.x, 5 - gbbox.y]).event(nodeGroup);
+        svg.attr("height", gbbox.height + 10);
+        svg.attr("width", gbbox.width + 10);
+      }
+    });
+
+    colaAdaptor.on("end", function (event) {
+      console.log("end");
+    });
   }
 }
 
